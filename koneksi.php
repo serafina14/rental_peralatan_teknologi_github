0d32429c5323acfba59711cc5c7baadad103de31
serafina14 <124250107@student.upnyk.ac.id>
2026-04-30 20:21:14 +0700

    nambahin koneksi database dan gitignore

---
+++ koneksi.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "rentaltech";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn){
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+echo "Koneksi berhasil";
+?>
