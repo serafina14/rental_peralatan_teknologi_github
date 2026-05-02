7204c97e279d9e3f61f896b0eb4cbcfee5562677
Widyjayati <widymarpaung55@gmail.com>
2026-05-02 22:21:35 +0700

    styling navbar & footer, update index

--- index.php
+++ index.php
@@ -3,44 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <!-- css sendiri -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    
     <title>RentalTech</title>
 </head>
-<body style="background: #f5f5f5;">
-    //bootstrap
+
+<body>
+
+    <!-- navbar -->
+    <?php include "includes/navbar.php"; ?>
+    <!-- hero section -->
+    <div class="content">
+
+    </div>
+
+    <!-- footer -->
+    <?php include "includes/footer.php"; ?>
+
+    <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-
-    //navbar
-    <div style="background:#0f172a; color:white; display:flex; justify-content:space-between; align-items:center; padding:15px 40px;">
-        <!-- logo & text -->
-        <div style="display:flex; align-items:center; gap:10px;">
-            <img src="" alt="logo" style="width:28px; height:28px; object-fit:contain;">
-            <span style="color:#f59e0b; font-weight:bold; font-size:18px;">RentalTech</span>
-        </div>
-
-        <!-- menu di navbar -->
-        <div style="display:flex; align-items:center; gap:20px;">
-            <a href="#" style="text-decoration:none; color:#cbd5f5; font-size:14px;">Beranda</a>
-            <a href="#" style="text-decoration:none; color:#cbd5f5; font-size:14px;">Katalog</a>
-            <a href="#" style="text-decoration:none; color:#cbd5f5; font-size:14px;">Login</a>
-            <a href="#" style="background:#f59e0b; color:black; padding:6px 12px; border-radius:6px; text-decoration:none; font-size:14px;">Daftar</a>
-        </div>
-    </div>
-
-
-
-
-
-
-
-    
-    //footer
-    <div style="background:#0f172a; color:white; text-align:center; padding:15px; font-size:12px;">
-        © 2026 RentalTech - Sewa Peralatan Teknologi
-    </div>
-
-
-
-
 </body>
 </html>
